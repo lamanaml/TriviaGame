05b042872d0540d33860432be49b7feab86f5887
lamana <lamanaml@gmail.com>
2019-08-01 18:06:56 -0400

    added the wins and losses logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
-
+var incorrect = 0;
+var correct = 0;
 var questionsArray = [
 {q: "What ingredient makes bread rise?", a: "Yeast",  choices: ["Eggs", "Wheat", "Yeast", "Baking Soda"]},
 {q: "What is the main ingredient used in guacamole?", a: "Avocado",  choices: ["Tomatoes", "Mayonaise", "Avocado", "Beans"]},
@@ -11,28 +12,30 @@ var questionsArray = [
 {q: "Fajitas are widely eaten in which country?", a: "Mexico",  choices: ["Argentina", "Mexico", "Equador", "Panama Canal"]},
 {q: "Which nuts are used to make marzipan?", a: "Almonds",  choices: ["Almonds", "Macademia Nuts", "Walnuts", "Brazil Nuts"]},
 {q: "Which vegetable is zucchini better known as?", a: "Courgette",  choices: ["Courgette", "Cucumber", "Patty Pan Squash", "Spaghetti Squash"]}
-]
-      
-       
+] 
 
 $("#startTimer").on("click",function() {
    $("#startTimer").remove();
    game.start()
-
+ $("#questionDiv").append("<h2>Questions</h2>")
     for(var i = 0; i < questionsArray.length; i++){
-      $("#questionDiv").append('<h2>'+questionsArray[i].q+'<h2>')
+      $("#questionDiv").append('<hr><p><b>'+questionsArray[i].q+'</b></p>')
       for(var j = 0; j < questionsArray[j].choices.length; j++){
-        $("#questionDiv").append(" <input type='radio' " + "name='question-" +i+ "' value='" + questionsArray[i].choices[j] + "' > " + questionsArray[i].choices[j])
+       
+        $("#questionDiv").append(" <input type='radio' " + "name='question-" +i+ "' value='" + questionsArray[i].choices[j] + "' > " + questionsArray[i].choices[j] )
       }
     }
-     $("#done").append('<button id="done">Done</button>');
+    
+     $("#startTimer").append('<hr><button id="done">Done</button>');
  })
 
-
+$("#done").on("click",function() {
+    game.done()
+});
 var game = {
-  correct: 0,
-  incorrect: 0,
-  counter: 5,
+  //correct: 0,
+  //incorrect: 0,
+  counter: 20,
   countdown: function(){
     game.counter--;
     $('#time').html("Time Remaining: " + game.counter);
@@ -62,22 +65,96 @@ var game = {
 
      $.each($("input[name='question-0']:checked"), function () {
          if ($(this).val() == questionsArray[0].a) {
-           console.log(true)
+           console.log("correct");
+        correct++;
+        } else {
+            incorrect++;
+            console.log("incorrect");
+        }
+    });
+    $.each($("input[name='question-1']:checked"), function () {
+         if ($(this).val() == questionsArray[1].a) {
+        correct++;
+        console.log("correct");
+        } else {
+            incorrect++;
+            console.log("incorrect");
+        }
+    });
+    $.each($("input[name='question-2']:checked"), function () {
+         if ($(this).val() == questionsArray[2].a) {
         correct++;
         } else {
             incorrect++;
         }
     });
-   this.result()
-    }
+    $.each($("input[name='question-3']:checked"), function () {
+         if ($(this).val() == questionsArray[3].a) {
+        correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($("input[name='question-4']:checked"), function () {
+         if ($(this).val() == questionsArray[4].a) {
+        correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($("input[name='question-5']:checked"), function () {
+         if ($(this).val() == questionsArray[5].a) {
+        correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($("input[name='question-6']:checked"), function () {
+         if ($(this).val() == questionsArray[6].a) {
+        correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($("input[name='question-7]:checked"), function () {
+         if ($(this).val() == questionsArray[7].a) {
+        correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($("input[name='question-8']:checked"), function () {
+         if ($(this).val() == questionsArray[8].a) {
+        correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($("input[name='question-9']:checked"), function () {
+         if ($(this).val() == questionsArray[9].a) {
+        correct++;
+        } else {
+            incorrect++;
+        }
+    });
+    $.each($("input[name='question-10']:checked"), function () {
+         if ($(this).val() == questionsArray[10].a) {
+        correct++;
+        } else {
+            incorrect++;
+        }
+    });
+      result()
 
+    }
   }
   function result() {
-    console.log("results function")
+    
     clearInterval(timer);
-    $("#showResults").append('<h3> Correct Answers: ' + this.correct + '</h3>');
-    $("#showResults").append('<h3> Incorrect Answers: ' + this.incorrect + '</h3>');
-    $("#showResults").append('<h3> Unanswered: ' + (questionsArray.length - (this.incorrect + this.correct)) + '</h3>');       
+     $("#time").clear
+    $("#time").html('<h3> Correct Answers: ' + correct + '</h3>');
+    $("#time").append('<h3> Incorrect Answers: ' + incorrect + '</h3>');
+    $("#time").append('<h3> Unanswered: ' + (questionsArray.length - (incorrect + correct)) + '</h3>');       
 }
 
    
